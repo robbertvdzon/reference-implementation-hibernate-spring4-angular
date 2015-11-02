34e1ef416ba2e6743ade83a8b92f1ec3ad062090
Robbert <robbert@vdzon.com>
2015-11-02 20:45:09 +0100

    mysql support removed

--- docker/build-contactdb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-docker build -t robbertvdzon/contactdb ./contactdb
-docker run -d -p 8080:8080 --name contactdb robbertvdzon/contactdb
--- docker/build-mysql.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t robbertvdzon/mysql ./mysql
-docker run -d -p 3080:80 -p 3306:3306 --name mysqldb robbertvdzon/mysql
